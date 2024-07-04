a16dbc9e486ec3050b37d8e2d1c414656e061c65
hangui11 <hangyao2002@gmail.com>
2024-07-04 17:05:47 +0200

    add react native app

--- app/index.tsx
+++ app/index.tsx
@@ -1,19 +1,28 @@
 import { StatusBar } from "expo-status-bar";
-import { Modal, Text, View } from "react-native";
+import { StyleSheet, Modal, Text, View } from "react-native";
 import { Link } from "expo-router";
 
 export default function App() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Aora!</Text>
+    <View style={styles.container}>
+      <Text style={styles.text_style}>Aora!</Text>
       <StatusBar style="auto"></StatusBar>
       <Link href="/profile" style={{color: 'blue'}}>Go to Profile</Link>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "white",
+    
+  },
+
+  text_style: {
+    fontSize: 24,
+    fontFamily: "SpaceMono-Regular",
+  },
+}); 
