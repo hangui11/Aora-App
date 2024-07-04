4fa3d234e6ee2cf3aa1ac29405e47e6517d97ef7
hang.yao <hang.yao@estudiantat.upc.edu>
2024-07-04 22:07:32 +0200

    fix fonts problems

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,22 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Modal, Text, View } from "react-native";
 import { Link } from "expo-router";
+import { useFonts } from "expo-font";
 
 export default function App() {
+  const [loaded] = useFonts({
+    "SpaceMono-Regular": require("../assets/fonts/SpaceMono-Regular.ttf"),
+  });
+
+  if (!loaded) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.text_style}>Aora!</Text>
       <StatusBar style="auto"></StatusBar>
-      <Link href="/profile" style={{color: 'blue'}}>Go to Profile</Link>
+      <Link href="/profile" style={styles.link_style}>Go to Profile</Link>
     </View>
   );
 }
@@ -23,6 +32,13 @@ const styles = StyleSheet.create({
 
   text_style: {
     fontSize: 24,
+    fontFamily: "SpaceMono-Regular",
+  },
+
+  link_style: {
+    textDecorationLine: "underline",
+    color: "blue",
+    fontSize: 20,
     fontFamily: "SpaceMono-Regular",
   },
 }); 
